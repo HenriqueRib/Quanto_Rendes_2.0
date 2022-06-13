b51263a0f23f59c2ae2c75ed9fe0eaf6d311b9df
HenriqueRib <ribeiro.henriquem@gmail.com>
2022-06-13 06:26:50 -0300

    Pagina Editar

--- lib/pages/page_editar.dart
+++ lib/pages/page_editar.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable, deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -17,31 +19,243 @@ class _PageEditarState extends State<PageEditar> {
 
   getIdEditar() async {
     final prefs = await SharedPreferences.getInstance();
-    final String? _idEditar = prefs.getString('id_editar'); // Recuperar
-    print(_idEditar);
-    print('_idEditar');
+    final int? _idEditar = prefs.getInt('id_editar'); // Recuperar
+  }
+
+  TextEditingController _controllerkmAtual = TextEditingController();
+  final TextEditingController _controllerValorLitro = TextEditingController();
+  final TextEditingController _controllerValorReais = TextEditingController();
+  final TextEditingController _controllerQtdLitrosAbastecido =
+      TextEditingController();
+  final TextEditingController _controllerPosto = TextEditingController();
+  final FocusNode _focusKmAtual = FocusNode();
+  final FocusNode _focusValorLitro = FocusNode();
+  final FocusNode _focusValorReais = FocusNode();
+  final FocusNode _focusQtdLitro = FocusNode();
+  final FocusNode _focusPosto = FocusNode();
+  String dropdownValue = 'Etanol';
+  String _textoResultado = "Edite seu abastecimento";
+
+  _limpar() {
+    setState(() {
+      _textoResultado = "Edite seu abastecimento";
+      _controllerkmAtual.clear();
+      _controllerValorLitro.clear();
+      _controllerValorReais.clear();
+      _controllerQtdLitrosAbastecido.clear();
+      _controllerPosto.clear();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.teal[200],
-        padding: const EdgeInsets.all(20),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 0),
-                  child: Image.asset(
-                    "images/carro-azul.png",
-                    width: 200,
-                    height: 150,
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        _focusKmAtual.unfocus();
+        _focusValorLitro.unfocus();
+        _focusValorReais.unfocus();
+        _focusQtdLitro.unfocus();
+        _focusPosto.unfocus();
+      },
+      child: Scaffold(
+        body: Container(
+          color: Colors.teal[900],
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 0,
+                    ),
+                    child: Text(
+                      _textoResultado,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  TextField(
+                    focusNode: _focusKmAtual,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Km Atual, ex: 118877.5",
+                      suffixIcon: IconButton(
+                        onPressed: () => _controllerkmAtual.clear(),
+                        icon: const Icon(
+                          Icons.clear,
+                        ),
+                      ),
+                    ),
+                    style: const TextStyle(color: Colors.white, fontSize: 19),
+                    controller: _controllerkmAtual,
+                  ),
+                  TextField(
+                    focusNode: _focusValorLitro,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Valor do Litro, ex: 5.14",
+                      suffixIcon: IconButton(
+                        onPressed: () => _controllerValorLitro.clear(),
+                        icon: const Icon(
+                          Icons.clear,
+                        ),
+                      ),
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 19,
+                    ),
+                    controller: _controllerValorLitro,
+                  ),
+                  TextField(
+                    focusNode: _focusValorReais,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Valor em reais, ex: 209.09",
+                      suffixIcon: IconButton(
+                        onPressed: () => _controllerValorReais.clear(),
+                        icon: const Icon(
+                          Icons.clear,
+                        ),
+                      ),
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 19,
+                    ),
+                    controller: _controllerValorReais,
+                  ),
+                  TextField(
+                    focusNode: _focusQtdLitro,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Qtd em litros abastecido, ex: 40.61",
+                      suffixIcon: IconButton(
+                        onPressed: () => _controllerQtdLitrosAbastecido.clear(),
+                        icon: const Icon(
+                          Icons.clear,
+                        ),
+                      ),
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 19,
+                    ),
+                    controller: _controllerQtdLitrosAbastecido,
+                  ),
+                  TextField(
+                    focusNode: _focusPosto,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Posto de combustível",
+                      suffixIcon: IconButton(
+                        onPressed: () => _controllerPosto.clear(),
+                        icon: const Icon(
+                          Icons.clear,
+                        ),
+                      ),
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 19,
+                    ),
+                    controller: _controllerPosto,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 5,
+                      left: 5,
+                      right: 5,
+                      bottom: 10,
+                    ),
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: dropdownValue,
+                      icon: const Icon(
+                        Icons.arrow_downward,
+                        color: Colors.white,
+                      ),
+                      elevation: 16,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.teal,
+                      ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>['Etanol', 'Gasolina']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 20, left: 15, right: 15),
+                        child: RaisedButton(
+                          color: Colors.teal,
+                          textColor: Colors.white,
+                          padding: const EdgeInsets.all(15),
+                          child: const Text(
+                            "Limpar info",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () => _limpar(),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 20,
+                          left: 15,
+                          right: 15,
+                        ),
+                        child: RaisedButton(
+                          color: Colors.teal,
+                          textColor: Colors.white,
+                          padding: const EdgeInsets.all(15),
+                          child: const Text(
+                            "Salvar",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          //onPressed: _salvarAbastecimento
+                          onPressed: () {
+                            // _salvarAbastecimento();
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //     builder: (context) => TelaResposta(
+                            //       valor: _textoResultado,
+                            //     ),
+                            //   ),
+                            // );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
