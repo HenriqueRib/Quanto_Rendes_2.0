11cd9a8d8a101b1fc89c99835e7b438ce0e18e5f
HenriqueRib <ribeiro.henriquem@gmail.com>
2022-07-25 15:14:01 -0300

    Adicionando imagem de background na tela menu

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:quanto/pages/home.dart';
+import 'package:quanto/widgets/menu_card.dart';
 
 class Menu extends StatefulWidget {
   const Menu({Key? key}) : super(key: key);
@@ -11,26 +14,178 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.teal[900],
-        padding: const EdgeInsets.all(20),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 0),
-                  child: Image.asset(
-                    "assets/img/carro-azul.png",
-                    width: 200,
-                    height: 150,
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/img/background.jpeg'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
+          Container(
+            width: 1,
+            height: 1,
+            color: Colors.blue,
+          ),
+          SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: RefreshIndicator(
+              onRefresh: () async {
+                var futures = <Future>[];
+                await Future.wait(futures);
+                return Future.delayed(const Duration(seconds: 0));
+              },
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      child: Column(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  Container(
+                                    child: Padding(
+                                      padding: EdgeInsets.only(top: 10),
+                                      child: Text(
+                                        "t2t2aaaaaaaaaa",
+                                        style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 40),
+                                    child: SizedBox(
+                                      height: 100,
+                                      child: ListView(
+                                        scrollDirection: Axis.horizontal,
+                                        children: List.generate(
+                                          2,
+                                          (index) {
+                                            return MenuCard(
+                                              index: index,
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 0),
+                                    child: Text("t2a"),
+                                  ),
+                                  Text("t1"),
+                                  Center(
+                                    child: Text(
+                                      "t2aaaaaaaaaa",
+                                      style: TextStyle(
+                                        fontSize: 50,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Padding(
+                                      padding: EdgeInsets.only(top: 100),
+                                      child: Text(
+                                        "t2t2aaaaaaaaaa",
+                                        style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 100),
+                                    child: Text("t2"),
+                                  ),
+                                  Text("t1"),
+                                  Center(
+                                    child: Text(
+                                      "t2aaaaaaaaaa",
+                                      style: TextStyle(
+                                        fontSize: 50,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Padding(
+                                      padding: EdgeInsets.only(top: 100),
+                                      child: Text(
+                                        "t2t2aaaaaaaaaa",
+                                        style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 100),
+                                    child: Text("t2"),
+                                  ),
+                                  Text("t1"),
+                                  Center(
+                                    child: Text(
+                                      "t2aaaaaaaaaa",
+                                      style: TextStyle(
+                                        fontSize: 50,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Padding(
+                                      padding: EdgeInsets.only(top: 100),
+                                      child: Text(
+                                        "t2t2aaaaaaaaaa",
+                                        style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 100),
+                                    child: Text("t2"),
+                                  ),
+                                  Text("t1"),
+                                  Center(
+                                    child: Text(
+                                      "t2aaaaaaaaaa",
+                                      style: TextStyle(
+                                        fontSize: 50,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/menu_card.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MenuCard extends StatelessWidget {
+  const MenuCard({Key? key, this.index}) : super(key: key);
+
+  final index;
+
+  @override
+  Widget build(BuildContext context) {
+    var url = 'imgs/header/userdefault.png';
+
+    return Padding(
+      padding: index == 0
+          ? EdgeInsets.symmetric(horizontal: .1.sw)
+          : EdgeInsets.only(right: .1.sw),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: AspectRatio(
+          aspectRatio: 1.7,
+          child: Stack(
+            children: [
+              SizedBox(
+                width: 1.sw,
+                child: Image.asset("assets/img/carro-azul.png"),
+              ),
+              Container(
+                height: 1.sh,
+                width: 1.sw,
+                decoration: const BoxDecoration(
+                  color: Colors.black54,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: .02.sh,
+                  horizontal: .05.sw,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Testando legallll",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.sp,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: .01.sh),
+                      child: Text(
+                        "Testando legallll Description",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w300,
+                          fontSize: 16.sp,
+                        ),
+                      ),
+                    ),
+                    const Align(
+                      alignment: Alignment.bottomRight,
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {},
+                  customBorder: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: SizedBox(
+                    height: 1.sh,
+                    width: 1.sw,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
